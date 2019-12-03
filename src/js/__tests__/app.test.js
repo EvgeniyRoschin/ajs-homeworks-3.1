798cd58376b07483e27ed937dd14902474c7b99a
Evgeniy Roschin <evgeny689@yandex.ru>
2019-12-03 09:08:25 +0400

    eslint errors fix

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable linebreak-style */
 import characterStatus from '../app';
 
 test('healthy', () => {
--- src/js/app.js
+++ src/js/app.js
@@ -6,22 +6,22 @@
 // console.log(sum([1, 2]));
 
 export default function characterStatus(character) {
-    let status = '';
-    if (character.health) {
-      switch (true) {
-        case (character.health > 50):
-          status = 'healthy';
-          break;
-        case (character.health < 51 && character.health > 14):
-          status = 'wounded';
-          break;
-        case (character.health < 15):
-          status = 'critical';
-          break;
-        default:
-          status = '';
-      }
+  let status = '';
+  if (character.health) {
+    switch (true) {
+      case (character.health > 50):
+        status = 'healthy';
+        break;
+      case (character.health < 51 && character.health > 14):
+        status = 'wounded';
+        break;
+      case (character.health < 15):
+        status = 'critical';
+        break;
+      default:
+        status = '';
     }
-  
-    return status;
   }
+
+  return status;
+}
